88db2b5defa26bd1c00c9146fb70ddf97fbc324c
Christoph Graf <christoph.graf.01@gmail.com>
2016-08-18 11:44:35 +0200

    fixed scene loading

---
+++ Assets/Scripts/GameStart.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class GameStart : MonoBehaviour {
+	
+	// Update is called once per frame
+	void Update () {
+	    if(Input.anyKeyDown)
+        {
+            PlayerPrefs.SetInt("IGJAM16_SCENE", 0);
+            PlayerPrefs.SetString("IGJAM16_LANG", "de");
+
+            SceneManager.LoadScene("Transition");
+        }
+    }
+}
---
+++ Assets/Scripts/Transition.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Transition : MonoBehaviour {
+
+    public Text title;
+    public Text subTitle;
+    public float delay;
+
+    private int sceneIndex;
+    
+
+    // Use this for initialization
+	IEnumerator Start () {
+        sceneIndex = PlayerPrefs.GetInt("IGJAM16_SCENE");
+        title.text += (" " + (sceneIndex + 1).ToString());
+        yield return new WaitForSeconds(delay);
+        SceneManager.LoadScene("Main");
+	}
+}
