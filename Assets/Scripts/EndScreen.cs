f8a8028687b973ac4ea6b41c9f6791ea412c06fd
Christoph Graf <christoph.graf.01@gmail.com>
2016-08-18 14:15:38 +0200

    change from b/w to w/b

--- Assets/Scripts/EndScreen.cs
+++ Assets/Scripts/EndScreen.cs
@@ -4,4 +4,19 @@ using UnityEngine.UI;
 
 public class EndScreen : MonoBehaviour {
 
+    public Text title;
+    public Text subTitle;
+
+
+    void Start()
+    {
+        string lang = PlayerPrefs.GetString("IGJAM16_LANG", "de");
+        if (lang == "de")
+            title.text = "ENDE";
+        else if(lang == "en")
+        {
+            title.text = "THE END";
+
+        }
+    }
 }
